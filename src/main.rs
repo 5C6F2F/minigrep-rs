1d736fba4a6ad17dccbc027589bf37462250e839
5C6F2F <5c6f2f@gmail.com>
2025-08-25 19:09:46 +0900

    一通り実装完了

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate mini_grep;
+
+use mini_grep::Config;
+use std::{env, process};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(err) = mini_grep::run(config) {
+        eprintln!("Application Error: {}", err);
+        process::exit(1);
+    }
 }
